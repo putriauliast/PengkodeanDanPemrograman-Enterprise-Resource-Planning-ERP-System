8bc383426a829113b850c42c5dd9c1c35ccbae86
putriauliast <116511961+putriauliast@users.noreply.github.com>
2023-12-14 09:32:57 +0700

    Add files via upload

---
+++ storage/framework/views/7e92c13f1f26d5df25bbb5d1284df6f31ba7aaca.php
@@ -0,0 +1,87 @@
+<?php $__env->startSection('content'); ?>
+
+
+<section class="material-half-bg">
+    <div class="cover"></div>
+</section>
+<section class="login-content">
+    <div class="logo">
+        <h1>Sales Erp</h1>
+    </div>
+    <div class="login-box">
+        <form class="login-form" method="POST" action="<?php echo e(route('login')); ?>">
+            <?php echo csrf_field(); ?>
+            <h3 class="login-head"><i class="fa fa-lg fa-fw fa-user"></i>SIGN IN</h3>
+            <div class="form-group">
+                <label class="control-label">USERNAME</label>
+                <input id="email" type="email" class="form-control <?php if ($errors->has('email')) :
+if (isset($message)) { $messageCache = $message; }
+$message = $errors->first('email'); ?> is-invalid <?php unset($message);
+if (isset($messageCache)) { $message = $messageCache; }
+endif; ?>" name="email" value="<?php echo e(old('email')); ?>" required autocomplete="email" autofocus>
+                <?php if ($errors->has('email')) :
+if (isset($message)) { $messageCache = $message; }
+$message = $errors->first('email'); ?>
+                <span class="invalid-feedback" role="alert">
+                    <strong><?php echo e($message); ?></strong>
+                </span>
+                <?php unset($message);
+if (isset($messageCache)) { $message = $messageCache; }
+endif; ?>
+            </div>
+            <div class="form-group">
+                <label class="control-label">PASSWORD</label>
+                 <input id="password" type="password" class="form-control <?php if ($errors->has('password')) :
+if (isset($message)) { $messageCache = $message; }
+$message = $errors->first('password'); ?> is-invalid <?php unset($message);
+if (isset($messageCache)) { $message = $messageCache; }
+endif; ?>" name="password" required autocomplete="current-password">
+                 <?php if ($errors->has('email')) :
+if (isset($message)) { $messageCache = $message; }
+$message = $errors->first('email'); ?>
+                 <span class="invalid-feedback" role="alert">
+                     <strong><?php echo e($message); ?></strong>
+                 </span>
+                 <?php unset($message);
+if (isset($messageCache)) { $message = $messageCache; }
+endif; ?>
+            </div>
+            <div class="form-group">
+                <div class="utility">
+                    <div class="animated-checkbox">
+                        <label>
+                            <input type="checkbox"><span class="label-text">Stay Signed in</span>
+                        </label>
+                    </div>
+                    <p class="semibold-text mb-2"><a href="#" data-toggle="flip">Forgot Password ?</a></p>
+                </div>
+            </div>
+            <div class="form-group btn-container">
+                <button class="btn btn-primary btn-block" type="submit"><i class="fa fa-sign-in fa-lg fa-fw"></i>SIGN IN</button>
+            </div>
+        </form>
+        <form class="forget-form" action="index.html">
+            <h3 class="login-head"><i class="fa fa-lg fa-fw fa-lock"></i>Forgot Password ?</h3>
+            <div class="form-group">
+                <label class="control-label">EMAIL</label>
+                <input class="form-control" type="text" placeholder="Email">
+            </div>
+            <div class="form-group btn-container">
+                <button class="btn btn-primary btn-block"><i class="fa fa-unlock fa-lg fa-fw"></i>RESET</button>
+            </div>
+            <div class="form-group mt-3">
+                <p class="semibold-text mb-0"><a href="#" data-toggle="flip"><i class="fa fa-angle-left fa-fw"></i> Back to Login</a></p>
+            </div>
+        </form>
+    </div>
+</section>
+
+
+
+
+
+
+
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.master', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\sia\ERPLaravel\resources\views/auth/login.blade.php ENDPATH**/ ?>
